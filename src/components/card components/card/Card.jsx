10a6a422780655d3cb9193a3c73dd4f1547c89c2
Saikat Das <saikatdas40g@gmail.com>
2024-07-19 14:33:29 +0530

    chore: Update Card component to use react-router-dom for navigation

--- src/components/card components/card/Card.jsx
+++ src/components/card components/card/Card.jsx
@@ -1,41 +1,25 @@
-<<<<<<< HEAD
+import { useNavigate } from "react-router-dom";
 import "./Card.css";
 
 const Card = ({ card }) => {
   const { id, title, src, organizedBy, Date } = card;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/card/${id}`);
+  };
+
   return (
-    <div className="card">
-      <div className="card-image">
+    <div class="card" onClick={() => handleClick()}>
+      <div class="card-image">
         <img src={src} alt={title} />
       </div>
-      <div className="heading">
+      <div class="heading">
         {" "}
         {title}
-        <div className="author">
+        <div class="author">
           {" "}
-          By <span className="name">{organizedBy}</span> {Date}
-=======
-import { useNavigate } from 'react-router-dom';
-import './Card.css';
-
-const Card = ({ card }) => {
-    const { id, title, src, organizedBy, Date } = card;
-    const navigate = useNavigate();
-
-    const handleClick = () => {
-
-        navigate(`/card/${id}`)
-    }
-
-    return (
-        <div class="card" onClick={()=> handleClick()}>
-            <div class="card-image">
-                <img src={src} alt={title} />
-            </div>
-            <div class="heading"> {title}
-                <div class="author"> By <span class="name">{organizedBy}</span> {Date}</div>
-            </div>
->>>>>>> 8f9c556d8ddf78d984bc91d4a7461a026dc0b4c9
+          By <span class="name">{organizedBy}</span> {Date}
         </div>
       </div>
     </div>
